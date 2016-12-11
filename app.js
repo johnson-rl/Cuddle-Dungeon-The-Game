ecd6ad75473f698d72b99aec828d842a17556ce6
johnson-rl <johnson.rl@gmail.com>
2016-12-11 01:00:35 -0800

    Iterator function now draws game grid and places objects on board.  Battle page created and styled. Next up: create first level, create comat mechanics, write script.

--- app.js
+++ app.js
@@ -5,60 +5,111 @@ var hero = {
   icon: 'src="cat_walk_new.gif"',
   placeHero: function (){
       placementClass = '.'+this.position.join('')
-      console.log(placementClass)
+      // console.log(placementClass)
       $('#hero').remove()
       $(placementClass).append('<img id="hero" '+hero.icon+'></img>')
     }
 }
-//coordinates are [column, row]
-function boardMaker(){
+
+function doNothing(){}
+
+function rowPlacer(row, nothing, side){
+  var rowPlacer = ('<div class="row '+row+'"></div>');
+  side.append(rowPlacer);
+}
+
+function columnPlacer(col, nothing, side){
+  var columnPlacer = ('<div class = "col-xs-1 '+col+'"></div>');
+  side.append(columnPlacer)
+}
+
+function placeObjects (colRow, board, nothing){
+  console.log(colRow);
+  $('.'+colRow).addClass(board[Number(colRow)]);
+  // debugger
+}
+
+function boardIterator(data, functionOne, functionTwo){
   var coordinate = [0,0]
-  console.log('boardMaker is running')
   for (;coordinate[1]<10; coordinate[1]++){
-    var classPlacer = ('<div class="row '+coordinate[1]+'"></div>');
-    console.log(classPlacer)
-    $('.left-half').append(classPlacer);
+    var row = coordinate[1]
+    functionOne(row, data, $('.left-half'));
     coordinate[0] = 0;
-    // debugger;
     for (;coordinate[0]<12; coordinate[0]++){
-      var columnPlacer = ('<div class = "col-xs-1 '+coordinate.join('')+'"></div>');
-      // console.log(coordinate,coordinateString)
-      $('.left-half').append(columnPlacer)
+      var colRow = coordinate.join('');
+      functionTwo(colRow, data, $('.left-half'))
     }
   }
   coordinate = [12,0];
   for (;coordinate[1]<10; coordinate[1]++){
-    var classPlacer = ('<div class="row '+coordinate[1]+'"></div>');
-    console.log(classPlacer)
-    $('.right-half').append(classPlacer);
+    var row = coordinate[1]
+    functionOne(row, data, $('.right-half'));
     coordinate[0] = 0;
-    // debugger;
     for (coordinate[0]=12;coordinate[0]<24; coordinate[0]++){
-      var columnPlacer = ('<div class = "col-xs-1 '+coordinate.join('')+'"></div>');
-      // console.log(coordinate,coordinateString)
-      $('.right-half').append(columnPlacer)
+      var colRow = coordinate.join('');
+      functionTwo(colRow, data, $('.right-half'))
     }
   }
 }
 
-
+//coordinates are [column, row]
+// function boardMaker(){
+//   var coordinate = [0,0]
+//   // console.log('boardMaker is running')
+//   for (;coordinate[1]<10; coordinate[1]++){
+//     var classPlacer = ('<div class="row '+coordinate[1]+'"></div>');
+//     // console.log(classPlacer)
+//     $('.left-half').append(classPlacer);
+//     coordinate[0] = 0;
+//     // debugger;
+//     for (;coordinate[0]<12; coordinate[0]++){
+//       var columnPlacer = ('<div class = "col-xs-1 '+coordinate.join('')+'"></div>');
+//       // console.log(coordinate,coordinateString)
+//       $('.left-half').append(columnPlacer)
+//     }
+//   }
+//   coordinate = [12,0];
+//   for (;coordinate[1]<10; coordinate[1]++){
+//     var classPlacer = ('<div class="row '+coordinate[1]+'"></div>');
+//     console.log(classPlacer)
+//     $('.right-half').append(classPlacer);
+//     coordinate[0] = 0;
+//     // debugger;
+//     for (coordinate[0]=12;coordinate[0]<24; coordinate[0]++){
+//       var columnPlacer = ('<div class = "col-xs-1 '+coordinate.join('')+'"></div>');
+//       // console.log(coordinate,coordinateString)
+//       $('.right-half').append(columnPlacer)
+//     }
+//   }
+// }
 
 $(document).on('ready', function(){
-  boardMaker()
+  // boardMaker()
+  boardIterator(null, rowPlacer, columnPlacer)
   hero.placeHero()
+  levelOne = new Level()
+  levelOne.buildLevel(levelOneData,levelOne)
+  boardIterator(levelOne, doNothing, placeObjects)
   $(window).keydown(function(e){
     // console.log("working",e.keycode)
     if (e.keyCode===37){
       hero.position[0]-=1;
-        if()
+      if(!$('.'+hero.position.join('')).hasClass('move'))
+      hero.position[0]+=1;
     } else if(e.keyCode===38) {
       hero.position[1]-=1;
+      if(!$('.'+hero.position.join('')).hasClass('move'))
+      hero.position[1]+=1;
     } else if(e.keyCode===39) {
       hero.position[0]+=1;
+      if(!$('.'+hero.position.join('')).hasClass('move'))
+      hero.position[0]-=1;
     } else if(e.keyCode===40) {
       hero.position[1]+=1;
+      if(!$('.'+hero.position.join('')).hasClass('move')||hero.position[1]>=10)
+      hero.position[1]-=1;
     }
-    console.log(hero.position)
+    // console.log(hero.position)
     hero.placeHero();
   })
 })
---
+++ data.js
@@ -0,0 +1,98 @@
+function Level () {
+   this[00] = "move";  this[01] = "move";  this[02] = "move";  this[03] = "move";  this[04] = "move";  this[05] = "move";  this[06] = "move";  this[07] = "move";  this[08] = "move";  this[09] = "move";
+   this[10] = "move";  this[11] = "move";  this[12] = "move";  this[13] = "move";  this[14] = "move";  this[15] = "move";  this[16] = "move";  this[17] = "move";  this[18] = "move";  this[19] = "move";
+   this[20] = "move";  this[21] = "move";  this[22] = "move";  this[23] = "move";  this[24] = "move";  this[25] = "move";  this[26] = "move";  this[27] = "move";  this[28] = "move";  this[29] = "move";
+   this[30] = "move";  this[31] = "move";  this[32] = "move";  this[33] = "move";  this[34] = "move";  this[35] = "move";  this[36] = "move";  this[37] = "move";  this[38] = "move";  this[39] = "move";
+   this[40] = "move";  this[41] = "move";  this[42] = "move";  this[43] = "move";  this[44] = "move";  this[45] = "move";  this[46] = "move";  this[47] = "move";  this[48] = "move";  this[49] = "move";
+   this[50] = "move";  this[51] = "move";  this[52] = "move";  this[53] = "move";  this[54] = "move";  this[55] = "move";  this[56] = "move";  this[57] = "move";  this[58] = "move";  this[59] = "move";
+   this[60] = "move";  this[61] = "move";  this[62] = "move";  this[63] = "move";  this[64] = "move";  this[65] = "move";  this[66] = "move";  this[67] = "move";  this[68] = "move";  this[69] = "move";
+   this[70] = "move";  this[71] = "move";  this[72] = "move";  this[73] = "move";  this[74] = "move";  this[75] = "move";  this[76] = "move";  this[77] = "move";  this[78] = "move";  this[79] = "move";
+   this[80] = "move";  this[81] = "move";  this[82] = "move";  this[83] = "move";  this[84] = "move";  this[85] = "move";  this[86] = "move";  this[87] = "move";  this[88] = "move";  this[89] = "move";
+   this[90] = "move";  this[91] = "move";  this[92] = "move";  this[93] = "move";  this[94] = "move";  this[95] = "move";  this[96] = "move";  this[97] = "move";  this[98] = "move";  this[99] = "move";
+  this[100] = "move"; this[101] = "move"; this[102] = "move"; this[103] = "move"; this[104] = "move"; this[105] = "move"; this[106] = "move"; this[107] = "move"; this[108] = "move"; this[109] = "move";
+  this[110] = "move"; this[111] = "move"; this[112] = "move"; this[113] = "move"; this[114] = "move"; this[115] = "move"; this[116] = "move"; this[117] = "move"; this[118] = "move"; this[119] = "move";
+  this[120] = "move"; this[121] = "move"; this[122] = "move"; this[123] = "move"; this[124] = "move"; this[125] = "move"; this[126] = "move"; this[127] = "move"; this[128] = "move"; this[129] = "move";
+  this[130] = "move"; this[131] = "move"; this[132] = "move"; this[133] = "move"; this[134] = "move"; this[135] = "move"; this[136] = "move"; this[137] = "move"; this[138] = "move"; this[139] = "move";
+  this[140] = "move"; this[141] = "move"; this[142] = "move"; this[143] = "move"; this[144] = "move"; this[145] = "move"; this[146] = "move"; this[147] = "move"; this[148] = "move"; this[149] = "move";
+  this[150] = "move"; this[151] = "move"; this[152] = "move"; this[153] = "move"; this[154] = "move"; this[155] = "move"; this[156] = "move"; this[157] = "move"; this[158] = "move"; this[159] = "move";
+  this[160] = "move"; this[161] = "move"; this[162] = "move"; this[163] = "move"; this[164] = "move"; this[165] = "move"; this[166] = "move"; this[167] = "move"; this[168] = "move"; this[169] = "move";
+  this[170] = "move"; this[171] = "move"; this[172] = "move"; this[173] = "move"; this[174] = "move"; this[175] = "move"; this[176] = "move"; this[177] = "move"; this[178] = "move"; this[179] = "move";
+  this[180] = "move"; this[181] = "move"; this[182] = "move"; this[183] = "move"; this[184] = "move"; this[185] = "move"; this[186] = "move"; this[187] = "move"; this[188] = "move"; this[189] = "move";
+  this[190] = "move"; this[191] = "move"; this[192] = "move"; this[193] = "move"; this[194] = "move"; this[195] = "move"; this[196] = "move"; this[197] = "move"; this[198] = "move"; this[199] = "move";
+  this[200] = "move"; this[201] = "move"; this[202] = "move"; this[203] = "move"; this[204] = "move"; this[205] = "move"; this[206] = "move"; this[207] = "move"; this[208] = "move"; this[209] = "move";
+  this[210] = "move"; this[211] = "move"; this[212] = "move"; this[213] = "move"; this[214] = "move"; this[215] = "move"; this[216] = "move"; this[217] = "move"; this[218] = "move"; this[219] = "move";
+  this[220] = "move"; this[221] = "move"; this[222] = "move"; this[223] = "move"; this[224] = "move"; this[225] = "move"; this[226] = "move"; this[227] = "move"; this[228] = "move"; this[229] = "move";
+  this[230] = "move"; this[231] = "move"; this[232] = "move"; this[233] = "move"; this[234] = "move"; this[235] = "move"; this[236] = "move"; this[237] = "move"; this[238] = "move"; this[239] = "move";
+}
+
+Level.prototype.buildLevel = function(builder, level){
+  var keys = Object.keys(builder);
+console.log(keys)
+  keys.forEach(function(key){
+level[key] = builder[key];
+  })
+}
+
+levelOneData = {
+  10 : 'rock',
+  11 : 'rock',
+  12 : 'rock',
+  13 : 'rock',
+  23 : 'rock',
+  24 : 'rock'
+}
+
+
+
+//Here is the boardIterator for reference purposes.
+// function boardIterator(data, functionOne, functionTwo){
+//   var coordinate = [0,0]
+//   for (;coordinate[1]<10; coordinate[1]++){
+//     var row = coordinate[1]
+//     functionOne(row, data, $('.left-half'));
+//     coordinate[0] = 0;
+//     for (;coordinate[0]<12; coordinate[0]++){
+//       var colRow = coordinate.join('');
+//       functionTwo(colRow, data, $('.left-half'))
+//     }
+//   }
+//   coordinate = [12,0];
+//   for (;coordinate[1]<10; coordinate[1]++){
+//     var row = coordinate[1]
+//     functionOne(row, data, $('.right-half'));
+//     coordinate[0] = 0;
+//     for (coordinate[0]=12;coordinate[0]<24; coordinate[0]++){
+//       var colRow = coordinate.join('');
+//       functionTwo(colRow, data, $('.right-half'))
+//     }
+//   }
+// }
+
+
+
+// var levelProto = {
+//    00 : "move",  01 : "move",  02 : "move",  03 : "move",  04 : "move",  05 : "move",  06 : "move",  07 : "move",  08 : "move",  09 : "move",
+//    10 : "move",  11 : "move",  12 : "move",  13 : "move",  14 : "move",  15 : "move",  16 : "move",  17 : "move",  18 : "move",  19 : "move",
+//    20 : "move",  21 : "move",  22 : "move",  23 : "move",  24 : "move",  25 : "move",  26 : "move",  27 : "move",  28 : "move",  29 : "move",
+//    30 : "move",  31 : "move",  32 : "move",  33 : "move",  34 : "move",  35 : "move",  36 : "move",  37 : "move",  38 : "move",  39 : "move",
+//    40 : "move",  41 : "move",  42 : "move",  43 : "move",  44 : "move",  45 : "move",  46 : "move",  47 : "move",  48 : "move",  49 : "move",
+//    50 : "move",  51 : "move",  52 : "move",  53 : "move",  54 : "move",  55 : "move",  56 : "move",  57 : "move",  58 : "move",  59 : "move",
+//    60 : "move",  61 : "move",  62 : "move",  63 : "move",  64 : "move",  65 : "move",  66 : "move",  67 : "move",  68 : "move",  69 : "move",
+//    70 : "move",  71 : "move",  72 : "move",  73 : "move",  74 : "move",  75 : "move",  76 : "move",  77 : "move",  78 : "move",  79 : "move",
+//    80 : "move",  81 : "move",  82 : "move",  83 : "move",  84 : "move",  85 : "move",  86 : "move",  87 : "move",  88 : "move",  89 : "move",
+//    90 : "move",  91 : "move",  92 : "move",  93 : "move",  94 : "move",  95 : "move",  96 : "move",  97 : "move",  98 : "move",  99 : "move",
+//   100 : "move", 101 : "move", 102 : "move", 103 : "move", 104 : "move", 105 : "move", 106 : "move", 107 : "move", 108 : "move", 109 : "move",
+//   110 : "move", 111 : "move", 112 : "move", 113 : "move", 114 : "move", 115 : "move", 116 : "move", 117 : "move", 118 : "move", 119 : "move",
+//   120 : "move", 121 : "move", 122 : "move", 123 : "move", 124 : "move", 125 : "move", 126 : "move", 127 : "move", 128 : "move", 129 : "move",
+//   130 : "move", 131 : "move", 132 : "move", 133 : "move", 134 : "move", 135 : "move", 136 : "move", 137 : "move", 138 : "move", 139 : "move",
+//   140 : "move", 141 : "move", 142 : "move", 143 : "move", 144 : "move", 145 : "move", 146 : "move", 147 : "move", 148 : "move", 149 : "move",
+//   150 : "move", 151 : "move", 152 : "move", 153 : "move", 154 : "move", 155 : "move", 156 : "move", 157 : "move", 158 : "move", 159 : "move",
+//   160 : "move", 161 : "move", 162 : "move", 163 : "move", 164 : "move", 165 : "move", 166 : "move", 167 : "move", 168 : "move", 169 : "move",
+//   170 : "move", 171 : "move", 172 : "move", 173 : "move", 174 : "move", 175 : "move", 176 : "move", 177 : "move", 178 : "move", 179 : "move",
+//   180 : "move", 181 : "move", 182 : "move", 183 : "move", 184 : "move", 185 : "move", 186 : "move", 187 : "move", 188 : "move", 189 : "move",
+//   190 : "move", 191 : "move", 192 : "move", 193 : "move", 194 : "move", 195 : "move", 196 : "move", 197 : "move", 198 : "move", 199 : "move",
+//   200 : "move", 201 : "move", 202 : "move", 203 : "move", 204 : "move", 205 : "move", 206 : "move", 207 : "move", 208 : "move", 209 : "move",
+//   210 : "move", 211 : "move", 212 : "move", 213 : "move", 214 : "move", 215 : "move", 216 : "move", 217 : "move", 218 : "move", 219 : "move",
+//   220 : "move", 221 : "move", 222 : "move", 223 : "move", 224 : "move", 225 : "move", 226 : "move", 227 : "move", 228 : "move", 229 : "move",
+//   230 : "move", 231 : "move", 232 : "move", 233 : "move", 234 : "move", 235 : "move", 236 : "move", 237 : "move", 238 : "move", 239 : "move"
+// }
